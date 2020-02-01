14a4c156401daa13d81288f846fc782db38e8cfd
Ayumi Osawa <ayumi@Ayumis-MacBook-Air.local>
2020-02-01 18:00:47 +0100

    footprints, feets following cursor

--- footprints/index.js
+++ footprints/index.js
@@ -1,26 +1,31 @@
 // Parameters
 const rate = 100;    // The rate of fetching the cursor location (ms)
 
-
 let timer;
+let counter = 0;
 let x; 
 let y;
 
 document.addEventListener("DOMContentLoaded", () => {
   const screen = document.querySelector("#screen");
-  // screen.addEventListener("mousemove", () => {
-  //   x = event.clientX;
-  //   y = event.clientY;
-  // })
- 
 
   timer = setInterval(() => {
-
   screen.addEventListener("mousemove", () => {
     x = event.clientX;
     y = event.clientY;
   })
   document.querySelector("#text").textContent = "X: " + x + ", Y: " + y;
-  }, 200);
+  counter++;
+  console.log(counter);
+
+  if (counter % 2 === 0) {
+    document.querySelector("#birdR").style.left = String(x) + "px";
+    document.querySelector("#birdR").style.top = String(y) + "px";
+  } else {
+    document.querySelector("#birdL").style.left = String(x) + "px";
+    document.querySelector("#birdL").style.top = String(y) + "px";
+  }
+
+  }, 500);
 
 })
