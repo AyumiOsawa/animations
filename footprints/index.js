367ffffce153175b2c9f9b49bd4b545df7173856
Ayumi Osawa <ayumi@Ayumis-MacBook-Air.local>
2020-02-02 22:16:24 +0100

    footprints, new images added, a frog joined and buttons added

--- footprints/index.js
+++ footprints/index.js
@@ -1,9 +1,9 @@
 "use strict";
 
-// Parameters
+// Parameter
 const speed = 500;    // The rate of fetching the cursor location & walking (ms)
 
-// Global varaiables?
+// other variables
 let counter = 0;
 let x;
 let y;
@@ -12,7 +12,7 @@ let y;
 const get_rotation_degree = (x, y, prevX, prevY) => {
   const diffX = x - prevX;
   const diffY = y - prevY;
-  const rawDeg = Math.atan2(diffY, diffX) * 180 / Math.PI;   // rad -> deg 
+  const rawDeg = Math.atan2(diffY, diffX) * 180 / Math.PI;   // rad -> deg. NOTE: The direction of y axis is opposite to math x-y plane. The returned value is +/- flipped. CSS rotation feature change the angle in clockwise direction whereas it's opposite in math degree system. Leave this +/- flipped value as it is to adjust to CSS system. 
   let degreeInCSS = rawDeg + 90;   // add 90 to start from 12:00 direction
   let moving;
   if (diffX === 0 && diffY === 0) {
@@ -31,10 +31,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const screen = document.querySelector("#screen");
   const vh = document.documentElement.clientHeight;
   const vw = document.documentElement.clientWidth;
+  const btn = document.querySelector(".btn");
+  const birdBtn = document.querySelector("#bird_btn");
+  const frogBtn = document.querySelector("#frog_btn");
   let degree = 0;
   let prevX = vw * 0.5; // default value at the center
   let prevY = vh * 0.5;
   let moving = false;
+
+  // EVENT: add a function to buttons to switch between animals
+  frogBtn.addEventListener("click", () => {
+    birdBtn.classList.toggle("btn--selected");
+    frogBtn.classList.toggle("btn--selected");
+  })
+  birdBtn.addEventListener("click", () => {
+    birdBtn.classList.toggle("btn--selected");
+    frogBtn.classList.toggle("btn--selected");
+  })
 
   // EVENT: getting the cursor position at every mousemove
   screen.addEventListener("mousemove", () => {
@@ -48,7 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // calculate the degree of foot pic rotation
     [degree, prevX, prevY, moving] = get_rotation_degree(x, y, prevX, prevY); 
     
-    // right, left, right, left, right ... 
+    // left, right, left, right... 
     counter++;
     console.log(counter);
     if (moving) {
