064048cfe7e23a39f668a254945792334aa96e38
AyumiOsawa <a.osawa1002@gmail.com>
2020-11-26 17:10:17 +0200

    rendering throttled

--- footprints/src/components/footprint.jsx
+++ footprints/src/components/footprint.jsx
@@ -8,20 +8,40 @@ import right_foot from '../bird_right.png';
 import left_foot from '../bird_left.png';
 
 function Footprint({isOn}) {
-  // let raw_x = useRef(center_x);
-  // let raw_y = useRef(center_y);
-  const center_x = Math.max(document.documentElement.clientWidth/2 || 0, window.innerWidth/2 || 0);
-  const center_y = Math.max(document.documentElement.clientHeight/2 || 0, window.innerHeight/2 || 0);
-  const average_speed = 1000;
+  let shouldThrottle = useRef(false);
+  // prev_x and prev_y are at the center at the beginning
+
+  let prev_x = useRef(Math.max(document.documentElement.clientWidth/2 || 0, window.innerWidth/2 || 0));
+  let prev_y = useRef(Math.max(document.documentElement.clientHeight/2 || 0, window.innerHeight/2 || 0) - 100);
+  let doorCenter_x = useRef(prev_x);
+  let doorCenter_y = useRef(prev_y);
+
+  const [position, setPosition] = useState({x: prev_x.current, y: prev_y.current});
+
+  const average_speed = 800;
   const R = document.getElementById('right_foot');
   const L = document.getElementById('left_foot');
+  const door = document.getElementById("door");
 
-  const [position, setPosition] = useState({'x': center_x, 'y': center_y});
-  let prev_x = useRef(center_x);
-  let prev_y = useRef(center_y);
-  let throttle = useRef(false);
+  const getDefaultPosition = (door) => {
+    const { left,
+            top,
+            right,
+            bottom } = door.getBoundingClientRect();
+    doorCenter_x.current = left + (right - left)/2;
+    doorCenter_y.current = bottom + (bottom - top)/2;
+    setPosition({'x': doorCenter_x.current, 'y': doorCenter_y.current});
+  }
+
+  useEffect(() => {
+    if (door) {
+      getDefaultPosition(door);
+    }
+  }, []);
 
   const _updatePicture = (degree) => {
+    console.log('update pic');
+    console.log('position.x', position.x,'position.y',position.y);
     R.classList.toggle('stepping_foot');
     L.classList.toggle('stepping_foot');
     R.style.top = `${position['y']}px`;
@@ -33,22 +53,14 @@ function Footprint({isOn}) {
   }
 
   const drawFootprint = () => {
-    // get the current points of cursor.
-    // raw_x & raw_y are constantly changing while the cursor is moving.
-    // const fixed_x = raw_x;
-    // const fixed_y = raw_y;
-    //
-    // setPosition({
-    //   'x': fixed_x,
-    //   'y': fixed_y,
-    // });
+    console.log('drawFootprint');
+    console.log('position.x', position.x,'position.y',position.y);
 
-    // check if the cursor has been moved from the previous check.
-    const diff_x = (prev_x.current - position.x) === 0;
-    const diff_y = (prev_y.current - position.y) === 0;
+    const diff_x = position.x - prev_x.current;
+    const diff_y = position.y - prev_y.current;
 
     // if the cursor is moving, update prev_x & prev_y and redraw.
-    if (diff_x || diff_y) {
+    if (diff_x !== 0 || diff_y !== 0) {
       prev_x.current = position.x;
       prev_y.current = position.y;
       const new_degree = ( Math.atan2(diff_y, diff_x) * 180 / Math.PI ) + 90;  // rad -> deg. NOTE: The direction of y axis is opposite to math x-y plane. The returned value is +/- flipped. CSS rotation feature change the angle in clockwise direction whereas it's opposite in math degree system. Leave this +/- flipped value as it is to adjust to CSS system.
@@ -56,28 +68,20 @@ function Footprint({isOn}) {
     }
   };
 
-  // const getCursor = event => {
-  //   // let raw_x = event.clientX;
-  //   // let raw_y = event.clientY;
-  //   setPosition({x: event.clientX, y: event.clientY});
-  // };
-
 // keeping an eye on the change of isOn
   useEffect(() => {
     console.log('isOn is toggled');
+    console.log('position.x', position.x,'position.y',position.y);
+
     const screen = document.querySelector('body');
     const speed = average_speed + (Math.random() - 0.5) * 200;
-    const getCursor = event => {
-      console.log('isOn is true and getCursor is running');
-      console.log('throttle should be f:', throttle);
 
-      if(!throttle.current) {
-        console.log('throttle should still be f:', throttle);
-        throttle.current = true;
-        console.log('inside throttle');
-        console.log('throttle should be t:', throttle);
+    const getCursor = event => {
+      if(!shouldThrottle.current) {
+        console.log('will throttle');
+        shouldThrottle.current = true;
         setPosition({x: event.clientX, y: event.clientY});
-        setTimeout(() => throttle.current = false, speed);
+        setTimeout(() => shouldThrottle.current = false, speed);
       }
     };
 
@@ -85,20 +89,26 @@ function Footprint({isOn}) {
       screen.addEventListener('mousemove', getCursor);
     } else {
       screen.removeEventListener('mousemove', getCursor);
+      console.log('reset the position');
+      door ? getDefaultPosition(door)
+      : setPosition({x: prev_x.current, y: prev_y.current})
     }
     return () => screen.removeEventListener('mousemove', getCursor);
   }, [isOn]);
 
 // keep updating while isOn is true, keeping an eye on the change of position
   useEffect(() => {
-    if(isOn) drawFootprint();
+    if(isOn) {
+      console.log('footprint redraw');
+      drawFootprint();
+    }
   }, [position]);
 
   return (
       <>
         <img
           src={right_foot}
-          className='component react--right_foot stepping_foot'
+          className='component react--right_foot foot stepping_foot'
           id='right_foot'
           style={isOn
                   ? {
@@ -114,7 +124,7 @@ function Footprint({isOn}) {
         />
         <img
           src={left_foot}
-          className='component react--left_foot'
+          className='component react--left_foot foot'
           id='left_foot'
           style={isOn
                   ? {
